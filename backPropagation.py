ed4659957c672d3a94764d27fc6b818976a586c0
Tanveer81 <tanvir01719@gmail.com>
2018-05-13 23:20:14 +0600

    asd

--- backPropagation.py
+++ backPropagation.py
@@ -4,11 +4,7 @@ global layers, neurons, attributes, classes
 neurons = []
 numberOfSample = 0
 dataSet = []
-neuralNetwork = []
-
-
-# print(myobjectx.variable)
-
+nn = []
 
 class neuron:
     def __init__(self, layer, position, inputs):
@@ -18,12 +14,7 @@ class neuron:
         self.weight = []
         self.delta = None
         self.out = None
-
-
-def backPropagation(dataset):
-    # print(dat)
-    print("Hello from a function")
-
+        self.net = None
 
 def init():
     # in parameter.txt 1st , 2nd and 3rd line has layers,attributes and classes and next lines has number of neurons
@@ -44,33 +35,33 @@ def init():
     neurons.append(attributes)
     for i in range(0, layers):
         neurons.append(int(file.readline()))
+    neurons.append(classes)
     print("Number Of Neurons in each layer : ")
     print(neurons)
     file.close()
+
     fp = open('test.txt')  # open file on read mode
     lines = fp.read().split("\n")  # create a list containing all lines
     fp.close()  # close file\
-
     for line in lines:
         tokenizer = line.split()
         dataSet.append(tokenizer)
         numberOfSample = numberOfSample + 1
     print("Number Of samples", end=" : ")
     print(numberOfSample)
-    for ii in range(0, numberOfSample):
-        for j in range(0, attributes + 1):
-            print(dataSet[ii][j], end=" ")
-        print("")
-    print(float(dataSet[0][0]))
+    # for ii in range(0, numberOfSample):
+    #     for j in range(0, attributes + 1):
+    #         print(dataSet[ii][j], end=" ")
+    #     print("")
+    # print(float(dataSet[0][0]))
     # backPropagation(dataset)
     # print(globals())
 
-
 def createNeurons():
-    global neuralNetwork, layers, neurons
+    global nn, layers, neurons
     print("creating Neurons")
 
-    for i in range(0, layers):
+    for i in range(0, layers+1):
         layerNeurons = []
         for j in range(0, neurons[i + 1]):
             n = neuron(i, j, neurons[i])
@@ -78,17 +69,27 @@ def createNeurons():
                 n.weight.append(random.random())
             # print(n.weight)
             layerNeurons.append(n)
-        neuralNetwork.append(layerNeurons)
+        nn.append(layerNeurons)
         # layerNeurons.clear()
 
     # for i in range(0, layers):
     #     for j in range(0, neurons[i + 1]):
-    #         print(neuralNetwork[i][j].layer, end=" , ")
-    #         print(neuralNetwork[i][j].position, end=" , ")
-    #         print(neuralNetwork[i][j].inputs, end=" , ")
-    #         print(neuralNetwork[i][j].weight)
+    #         print(nn[i][j].layer, end=" , ")
+    #         print(nn[i][j].position, end=" , ")
+    #         print(nn[i][j].inputs, end=" , ")
+    #         print(nn[i][j].weight)
     #     print()
 
+
+def fwdPropagation(input):
+    for i in range(0, layers+1):
+        for j in range(0, neurons[i + 1]):
+            nn[i][j].net = 
+
+
+def backPropagation():
+    # print(dat)
+    print("Hello from a function")
 
 def main():
     init()
